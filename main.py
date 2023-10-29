2bbd6b02f8cd6b16fade87cf18a65dcd56372e31
Kovács József Miklós <kovacsjozsef7u@gmail.com>
2023-10-29 15:44:28 +0100

    feat: add Factory class and basic options

--- main.py
+++ main.py
@@ -35,9 +35,52 @@ class Warehouse:
             print(f"{k} : {v}")
 
 
+class Factory:
+    def __init__(self, name, price, production_volume) -> None:
+        self.quantity = 0
+        self.name = name
+        self.price = price
+        self.production_volume = production_volume
+
+    def produceCookie(self, warehouse: Warehouse) -> None:
+        warehouse.cookies["Cookie"] += self.quantity * self.production_volume
+
+
 class Player:
-    def __init__(self, warehouse) -> None:
+    def __init__(self, warehouse, factories) -> None:
         self.warehouse = warehouse
+        self.factories = factories
+
+    def buy_factory(self, name, quantity) -> None:
+        if not name in self.factories:
+            raise KeyError("There's no such factory!")
+        if quantity < 1:
+            raise ValueError("The quantity must be a positive number!")
+
+        total_price = quantity * self.factories[name].price
+
+        if self.warehouse.cookies["Cookie"] >= total_price:
+            self.factories[name].quantity += quantity
+            self.warehouse.cookies["Cookie"] -= total_price
+        else:
+            raise ValueError("You don't have enough Cookies!")
+
+    def sell_factory(self, name, quantity) -> None:
+        if not name in self.factories:
+            raise KeyError("There's no such factory!")
+        if quantity < 1:
+            raise ValueError("The quantity must be a positive number!")
+
+        if self.factories[name].quantity >= quantity:
+            total_price = quantity * self.factories[name].price
+            self.warehouse.cookies["Cookie"] += total_price
+            self.factories[name].quantity -= quantity
+        else:
+            raise ValueError("You can't sell more than you have!")
+
+    def list_factories(self) -> None:
+        for factory in self.factories.values():
+            print(f"\t{factory.name}: {factory.quantity}")
 
     def create_cookie(self) -> None:
         while True:
@@ -51,14 +94,51 @@ class Player:
                     print("Write 'cookie' or 'back'/'b'")
 
 
+def factory_menu(player: Player):
+    while True:
+        print("\n~Factory~")
+        player.list_factories()
+
+        try:
+            match input("Choice: ").split():
+                case ["buy", name, quantity]:
+                    player.buy_factory(name.capitalize(), int(quantity))
+                case ["sell", name, quantity]:
+                    player.sell_factory(name.capitalize(), int(quantity))
+                case ["back"] | ["b"]:
+                    break
+                case _:
+                    print(
+                        "Write 'back'/'b' or for example",
+                        "\n'buy takodachi 1'",
+                        "\n<buy/sell> <factory-name> <quantity>",
+                    )
+        except ValueError as e:
+            if "invalid literal for int() with base 10" in str(e):
+                print("The quantity must be a whole number!")
+            else:
+                print(e)
+        except KeyError as e:
+            print(e)
+
+
 def main() -> None:
-    player = Player(Warehouse())
+    player = Player(
+        Warehouse(),
+        {
+            "Takodachi": Factory("Takodachi", 5, 1),
+            "Robot": Factory("Robot", 25, 10),
+            "Farm": Factory("Farm", 500, 10),
+            "Mine": Factory("Mine", 5000, 125),
+        },
+    )
 
     while True:
         print(
             "\n~Menu~",
             "1. Warehouse",
             "2. Create cookie",
+            "3. Factory",
             "exit - Exit",
             sep="\n\t",
         )
@@ -70,6 +150,8 @@ def main() -> None:
                 player.warehouse.list_commodities()
             case "2":
                 player.create_cookie()
+            case "3":
+                factory_menu(player)
             case _:
                 print("..?")
 
