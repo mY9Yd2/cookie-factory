59ca889a19c3f8a04812d4c177b6eea293994872
Kovács József Miklós <kovacsjozsef7u@gmail.com>
2023-12-17 19:02:55 +0100

    refactor: remove shop.py - move type_of_currency to their respective Enums - move effect price to PurchasableEffect - move the rest of the shop functionality to the Player class

--- player.py
+++ player.py
@@ -24,7 +24,27 @@ from collections import Counter
 
 from cookie import Cookie
 from factory import Factory
-from effect import EffectFn
+from effect import EffectFn, PurchasableEffect
+
+
+class NotEnoughCookie(Exception):
+    pass
+
+
+class NotPositiveNumber(Exception):
+    pass
+
+
+class TooMuchCookie(Exception):
+    pass
+
+
+class NotEnoughFactory(Exception):
+    pass
+
+
+class EffectAlreadyExist(Exception):
+    pass
 
 
 class Player:
@@ -32,3 +52,67 @@ class Player:
         self.cookies: Counter[Cookie] = Counter()
         self.factories: Counter[Factory] = Counter()
         self.effects: set[EffectFn] = set()
+
+    def get_next_factory_price(self, initial_quantity: int, base_price: int) -> int:
+        try:
+            return round(base_price * (1.15**initial_quantity))
+        except OverflowError as error:
+            raise TooMuchCookie("Too much!") from error
+
+    def buy_factory(self, item: Factory, quantity: int) -> int:
+        if quantity < 1:
+            raise NotPositiveNumber("The quantity must be a positive number!")
+
+        total_price = 0
+        for q in range(quantity):
+            initial_quantity = self.factories[item] + q
+            total_price += self.get_next_factory_price(
+                initial_quantity, item.base_price
+            )
+
+        if total_price > self.cookies[item.type_of_currency]:
+            message = f"You don't have enough {item.type_of_currency}!"
+            message += f"\nIt costs {total_price} {item.type_of_currency}"
+
+            raise NotEnoughCookie(message)
+
+        self.cookies[item.type_of_currency] -= total_price
+        self.factories[item] += quantity
+
+        return total_price
+
+    def sell_factory(self, item: Factory, quantity: int) -> int:
+        if quantity < 1:
+            raise NotPositiveNumber("The quantity must be a positive number!")
+
+        if self.factories[item] - quantity < 0:
+            raise NotEnoughFactory("You can't sell more than you have!")
+
+        total_price = 0
+        for q in range(quantity):
+            initial_quantity = self.factories[item] - quantity + q
+            total_price += self.get_next_factory_price(
+                initial_quantity, item.base_price
+            )
+
+        self.cookies[item.type_of_currency] += total_price
+        self.factories[item] -= quantity
+
+        return total_price
+
+    def buy_effect(self, item: PurchasableEffect) -> int:
+        if item.function in self.effects:
+            raise EffectAlreadyExist("You already bought this!")
+
+        price = item.base_price
+
+        if price > self.cookies[item.type_of_currency]:
+            message = f"You don't have enough {item.type_of_currency}!"
+            message += f"\nIt costs {price} {item.type_of_currency}"
+
+            raise NotEnoughCookie(message)
+
+        self.cookies[item.type_of_currency] -= price
+        self.effects.add(item.function)
+
+        return price
